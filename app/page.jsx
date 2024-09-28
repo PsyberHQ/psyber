d4913b8e4bcce85274cafdd9df9c4da5d4c0e534
Emmanuel Bassey <bassey214@gmail.com>
2024-09-28 23:49:37 +0100

    changes made

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Header from "@/components/Home/Header";
 import Section1 from "@/components/Home/Section1";
 import Section2 from "@/components/Home/Section2";
 import Section3 from "@/components/Home/Section3";
-import Section4 from "@/components/Home/Section4";
+import Section4B from "@/components/Home/Section4B";
 import Section5 from "@/components/Home/Section5";
 import Section6 from "@/components/Home/Section6";
 import Section7 from "@/components/Home/Section7";
@@ -16,7 +16,7 @@ export default function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
-      <Section4 />
+      <Section4B />
       <Section5 />
       <Section6 />
       <Section7 />
--- components/Home/Section1.jsx
+++ components/Home/Section1.jsx
@@ -5,36 +5,39 @@ const Section1 = () => {
   return (
     <section className="relative md:px-[4vw] z-0 px-[3vw] bg-[#FEFCFFAB]  h-fit pb-12 pt-28">
       <Image
-        src="/homeSection1Bg.jpeg"
+        src="/homeSection1Bg.png"
         alt="Logo"
         layout="fill"
         className="absolute -z-10 object-fill opacity-40"
       />
-      <div className="w-full h-fit grid grid-cols-2 gap-[30px]">
-        <div className="flex flex-col justify-center items-start">
-          <h1 className="text-[48px] font-[600] leading-[57px] text-brand-darkbrown">
-            Feeling Lost in the Web3 Buzz? Don&rsquo;t Worry, We&rsquo;ve Got
-            You
-          </h1>
-          <br />
-          <p className="font-[400] text-[16px] leading-[23px] text-brand-darkbrown">
-            Whether you&rsquo;re a professional or just someone who loves
-            staying updated, Psyber helps you smoothly navigate Web3 while
-            prioritizing your mental well-being
-          </p>
-          <br />
 
-          <Link href="/">
-            <button className="bg-brand-button-purple w-[212px] h-[58px] text-[20px] rounded-[42px] text-white font-semibold  px-4 ">
-              Explore Psyber
-            </button>
-          </Link>
-        </div>
+      <div className="w-full h-fit grid grid-cols-1 md:grid-cols-2 gap-2 items-center  md:mt-0 mt-[-10px]">
+      <div className="">
+        <h1 className=" text-2xl md:text-4xl lg:text-[48px] font-gloock font-[600]  sm:leading-[36px] md:leading-[48px] lg:leading-[57px] text-brand-darkbrown mb-[18px]">
+          Feeling Lost in the Web3 Buzz? Don&rsquo;t Worry, We&rsquo;ve Got You
+        </h1>
 
-        <div className="size-full">
-          <Image src="/girl1.png" alt="Logo" width={850} height={900} />
-        </div>
+        <p className="sm:w-full w-[80%] font-[400] font-urbanist text-[12px] sm:text-sm md:text-base lg:leading-relaxed text-brand-darkbrown">
+          Whether you&rsquo;re a professional or just someone who loves staying updated, Psyber helps you smoothly navigate Web3 while prioritizing your mental well-being.
+        </p>
+        <br />
+        <Link href="/">
+          <button className="shadow-custom-light bg-gradient-to-br from-[#efbff6] to-[#731981] w-[140px] md:w-[212px] h-[40px] md:h-[50px] sm:text-[16px] text-[14px] md:text-[20px] rounded-[32px] md:rounded-[42px] text-white font-semibold md:px-4 px-[10px] ">
+            Explore Psyber
+          </button>
+        </Link>
       </div>
+
+      <div className=" ">
+        <Image
+          src="/girl1.png"
+          alt="Logo"
+          width={850}
+          height={900}
+        />
+      </div>
+      </div>
+
     </section>
   );
 };
