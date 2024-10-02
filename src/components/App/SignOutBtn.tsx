8dc4d3eefe969593d16dd2c86636d896be8814cd
Ankush Makkar <ankushmakkar108@gmail.com>
2024-10-03 01:28:43 +0530

    task on app & dummy task

--- src/components/App/SignOutBtn.tsx
+++ src/components/App/SignOutBtn.tsx
@@ -4,6 +4,7 @@ import { signOut } from 'next-auth/react';
 const SignOutBtn = () => {
   return (
     <button
+    title='You better not click'
       className="flex w-fit max-w-xs items-center justify-center rounded-full bg-brand-button-purple px-4 py-2 font-semibold text-white hover:bg-[#933ba3]"
       onClick={() => {
         signOut();
---
+++ src/lib/const/fullTasks.ts
@@ -0,0 +1,171 @@
+const Fulltasks = [
+  {
+    id: 1,
+    title: 'Introduction to blockchain & web 3.0',
+    lessons: [
+      {
+        id: 1,
+        title: 'Introduction to blockchain',
+        content: [
+          {
+            id: 1,
+            type: 'text',
+            heading: 'Overview',
+            content:
+              'Blockchain is a digital ledger that securely records transactions. Instead of being controlled by a single entity, it’s shared among multiple participants, making it nearly impossible to alter without everyone noticing.',
+          },
+          {
+            id: 2,
+            type: 'text',
+            heading: 'Real Life Example',
+            content:
+              'Imagine a group of students working on a shared google doc. If one person makes a change, everyone else can see it, ensuring transparency.',
+          },
+          {
+            id: 2,
+            type: 'text',
+            heading: 'How it’s different and better than web 2.0',
+            content:
+              'Unlike web 2.0 , where data is managed and controlled by a single entity, blockchain distributes control among many, reducing the risk of tampering and increasing security.',
+          },
+          {
+            id: 3,
+            type: 'text',
+            heading: 'Key takeaway',
+            content:
+              'While web2 is dominated by a few big companies that control user data, Web 3.0 allows users to own their data and participate directly in the digital economy.',
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Introduction to blockchain p2',
+        content: [
+          {
+            id: 1,
+            type: 'text',
+            heading: 'Overview p2',
+            content:
+              'Blockchain is a digital ledger that securely records transactions. Instead of being controlled by a single entity, it’s shared among multiple participants, making it nearly impossible to alter without everyone noticing.',
+          },
+          {
+            id: 2,
+            type: 'text',
+            heading: 'Real Life Example p2',
+            content:
+              'Imagine a group of students working on a shared google doc. If one person makes a change, everyone else can see it, ensuring transparency.',
+          },
+          {
+            id: 2,
+            type: 'text',
+            heading: 'How it’s different and better than web 2.0 p2',
+            content:
+              'Unlike web 2.0 , where data is managed and controlled by a single entity, blockchain distributes control among many, reducing the risk of tampering and increasing security.',
+          },
+          {
+            id: 3,
+            type: 'text',
+            heading: 'Key takeaway p2',
+            content:
+              'While web2 is dominated by a few big companies that control user data, Web 3.0 allows users to own their data and participate directly in the digital economy.',
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: 'Introduction to blockchain p3',
+        content: [
+          {
+            id: 1,
+            type: 'text',
+            heading: 'Overview p3',
+            content:
+              'Blockchain is a digital ledger that securely records transactions. Instead of being controlled by a single entity, it’s shared among multiple participants, making it nearly impossible to alter without everyone noticing.',
+          },
+          {
+            id: 2,
+            type: 'text',
+            heading: 'Real Life Example p3',
+            content:
+              'Imagine a group of students working on a shared google doc. If one person makes a change, everyone else can see it, ensuring transparency.',
+          },
+          {
+            id: 2,
+            type: 'text',
+            heading: 'How it’s different and better than web 2.0 p3',
+            content:
+              'Unlike web 2.0 , where data is managed and controlled by a single entity, blockchain distributes control among many, reducing the risk of tampering and increasing security.',
+          },
+          {
+            id: 3,
+            type: 'text',
+            heading: 'Key takeaway p3',
+            content:
+              'While web2 is dominated by a few big companies that control user data, Web 3.0 allows users to own their data and participate directly in the digital economy.',
+          },
+        ],
+      },
+    ],
+    quiz: [
+      {
+        id: 1,
+        question: 'What is blockchain?',
+        options: [
+          {
+            id: 1,
+            content: 'A digital ledger that securely records transactions.',
+          },
+          {
+            id: 2,
+            content: 'A digital ledger that securely records transactions.',
+          },
+          {
+            id: 3,
+            content: 'A digital ledger that securely records transactions.',
+          },
+        ],
+        correctAnswer: 1,
+      },
+      {
+        id: 2,
+        question: 'What is blockchain? q2',
+        options: [
+          {
+            id: 1,
+            content: 'A digital ledger that securely records transactions.',
+          },
+          {
+            id: 2,
+            content: 'A digital ledger that securely records transactions.',
+          },
+          {
+            id: 3,
+            content: 'A digital ledger that securely records transactions.',
+          },
+        ],
+        correctAnswer: 1,
+      },
+      {
+        id: 3,
+        question: 'What is blockchain? q3',
+        options: [
+          {
+            id: 1,
+            content: 'A digital ledger that securely records transactions.',
+          },
+          {
+            id: 2,
+            content: 'A digital ledger that securely records transactions.',
+          },
+          {
+            id: 3,
+            content: 'A digital ledger that securely records transactions.',
+          },
+        ],
+        correctAnswer: 1,
+      },
+    ],
+  },
+];
+
+export default Fulltasks;
